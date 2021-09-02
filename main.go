ad299ff709bf446c8a82f338ac75129ae2bf6e32
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2021-09-02 23:03:42 +0300

    feat(handler): init handlers

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"go-mongo-rest-api/common"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":3000", router))
+}
+
+func startServer() {
+	common.Init()
+	handleRequests()
+}
+
+func main() {
+	fmt.Println("Server starting ...")
+	startServer()
+}
